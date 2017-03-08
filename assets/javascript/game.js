cace068e6cf3846dd5ee9d72aa74329e44a6acf1
Mickey S <marimikaels@gmail.com>
2017-03-07 21:42:15 -0500

    game logic basically fully works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,12 @@
+
+
+
 var possibleWords = ["vespa", "takkun", "mamimi", "haruko", "canti", "atomsk", "guitar", "alien", "ninamori", "baseball", "robot", "cat", "curry", "amarao", "bakery", "mabase", "kamon", "naota", "camera"];
 var numGuesses = 10;
 var numWins = 0;
 var lettersGuessed = [];
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var GAME_STATE = false;
 
 
 //get a random word from the posible choices
@@ -26,78 +30,155 @@ function generateBlanks(myWord) {
 }
 //check if there are blanks in an array
 function checkForBlanks(myArray) {
-	var arethereBlanks = false;
-	for (i=0, i <myArray.length; i++) {
-		if myArray[i] === "_" {
-			arethereBlanks = true;
-		}
-	}
-	return arethereBlanks;
+    var arethereBlanks = false;
+    for (i = 0; i < myArray.length; i++) {
+        if (myArray[i] === "_") {
+            arethereBlanks = true;
+        }
+    }
+    return arethereBlanks;
 }
+// game init
+//     ((player moves))
+//     -- > player wins-- > game ends
+//     -- > player loses-- > game ends
+//     -- > error-- > game ends - > game init
 
+
+
+
+
+
+
+//choose the secret word and show the corresponding blanks
+// var secretword = getWord(possibleWords);
+// var secretwordArray = generateBlanks(secretword);
+// $("#currentWord").html("Current Word: " + "<br>" + secretwordArray.join(" "));
+
+var secretword;
+var secretwordArray;
 
 function playGame() {
 
-
-    //choose the secret word and show the corresponding blanks
-    var secretword = getWord(possibleWords);
-    var secretwordArray = generateBlanks(secretword);
-    $("#currentWord").html("Current Word: " + "<br>" + secretwordArray.join(" "));
-
-
-    //when user presses a key:
-    document.onkeyup = function(event) {
-
-            var userGuess = event.key;
-            var lowerGuess = userGuess.toLowerCase();
-            if (alphabet.indexOf(lowerGuess) > -1 && lettersGuessed.indexOf(lowerGuess) === -1 && numGuesses>0 && checkForBlanks(secretwordArray) === true) { //only does game stuff if the key pressed is an alphabet key that has not already been guessed, and the user still has guesses left, and there are still blanks in the secret word array
-                if (secretword.indexOf(lowerGuess) > -1) { //if the guessed letter is in the secret word
-                    for (i = 0; i < secretword.length; i++) {
-                        if (secretword.charAt[i] === lowerGuess) {
-                            secretwordArray[i] = lowerGuess; //replace spaces in array of blanks with the letter
-                        }
-
-                    }
-
-                    
-                    lettersGuessed.push(lowerGuess);
-                    
-
-                    $("#currentWord").html("Current Word: " + "<br>" + secretwordArray.join(" ")); //display updated blanks array
-                    $("#lettersGuessed").html("Letters Guessed: " + "<br>" + lettersGuessed.join(" "));//display updated letters guessed array
+	if (GAME_STATE === false) {
+        secretword = getWord(possibleWords);
+        secretwordArray = generateBlanks(secretword);
+        $("#currentWord").html("Current Word: " + "<br>" + secretwordArray.join(" "));
+        GAME_STATE = true;
 
 
+    }
+//when user presses a key:
+document.onkeyup = function(event) {
+
+    if (GAME_STATE === false) {
+        secretword = getWord(possibleWords);
+        secretwordArray = generateBlanks(secretword);
+        $("#currentWord").html("Current Word: " + "<br>" + secretwordArray.join(" "));
+        GAME_STATE = true;
+
+
+    }
+
+    if (GAME_STATE === true) {
+
+    var userGuess = event.key;
+    var lowerGuess = userGuess.toLowerCase();
+    console.log(lowerGuess);
+    console.log(checkForBlanks(secretwordArray));
+    if (alphabet.indexOf(lowerGuess) > -1 && lettersGuessed.indexOf(lowerGuess) === -1 && numGuesses > 0 && checkForBlanks(secretwordArray) === true) { //only does game stuff if the key pressed is an alphabet key that has not already been guessed, and the user still has guesses left, and there are still blanks in the secret word array
+        if (secretword.indexOf(lowerGuess) > -1) { //if the guessed letter is in the secret word
+        	console.log("yes");
+        	console.log(secretword);
+        	console.log(secretword[0]);
+        	console.log(secretwordArray);
+            for (i = 0; i < secretword.length; i++) {
+                if (secretword[i] === lowerGuess) {
+                	console.log(secretword[i]);
+                    secretwordArray[i] = lowerGuess; //replace spaces in array of blanks with the letter
+                    console.log(secretwordArray);
                 }
 
-                else { //if the guessed letter is not in the secret word
-
-                	numGuesses = numGuesses - 1;
-                	$("#guessesRemaining").html("Guesses Remaining: " + "<br>" + numGuesses);//display updated number of guesses
-                	lettersGuessed.push(lowerGuess);
-                	$("#lettersGuessed").html("Letters Guessed: " + "<br>" + lettersGuessed.join(" "));//display updated letters guessed array
-                }
-
-
             }
 
-            if (numGuesses === 0) { //game over, user loses
-            	$("#gameOutcome").html("You Lose! Secret word was " + secretword);
-            	//game restarts (CODE GOES HERE)
+
+            lettersGuessed.push(lowerGuess);
 
 
-            }
+            $("#currentWord").html("Current Word: " + "<br>" + secretwordArray.join(" ")); //display updated blanks array
+            $("#lettersGuessed").html("Letters Guessed: " + "<br>" + lettersGuessed.join(" ")); //display updated letters guessed array
 
-            if (checkForBlanks(secretwordArray) === false) { //game over, user wins
-            	$("#gameOutcome").html("You Win! Secret word was " + secretword);
-            	numWins = numWins + 1;
-            	$("#numWins").html("Wins: " + numWins);
-            	//game restarts (CODE GOES HERE)
-            }
 
+        } else { //if the guessed letter is not in the secret word
+
+        	console.log("no");
+
+            numGuesses = numGuesses - 1;
+            $("#guessesRemaining").html("Guesses Remaining: " + numGuesses); //display updated number of guesses
+            lettersGuessed.push(lowerGuess);
+            $("#lettersGuessed").html("Letters Guessed: " + "<br>" + lettersGuessed.join(" ")); //display updated letters guessed array
         }
 
 
-function gameRestart() {
-	
+    }
 
 }
+
+    function gameEnd() {
+
+        numGuesses = 10;
+        $("#guessesRemaining").html("Guesses Remaining: " + "<br>" + numGuesses);
+        lettersGuessed = [];
+        $("#lettersGuessed").html("Letters Guessed: " + "<br>" + lettersGuessed.join(" "));
+        //$("#gameOutcome").empty();
+        console.log("game is over");
+
+        GAME_STATE = false;
+
+
+    }
+
+    if (numGuesses === 0) { //game over, user loses
+    	console.log("it's a loss");
+        $("#lastgameOutcome").html("Previous Game Outcome:" + "<br>"+ "You Lost! Secret word was " + secretword);
+        gameEnd(); //auto restart game on loss
+        playGame();
+
+
+    }
+
+    if (checkForBlanks(secretwordArray) === false) { //game over, user wins
+    	console.log("it's a win");
+        $("#lastgameOutcome").html("Previous Game Outcome:" + "<br>"+"You Won! Secret word was " + secretword);
+        numWins = numWins + 1;
+        $("#numWins").html("Wins: " + numWins);
+        gameEnd(); //auto restart game on win
+        playGame();
+    }
+
+}
+}
+playGame();
+
+
+
+
+// playGame();
+
+//document.addEventListener(onkeyup) {
+    //playGame();
+//}
+
+
+//function gameRestart() {
+
+//numGuesses = 10;
+//$("#guessesRemaining").html("Guesses Remaining: " + "<br>" + numGuesses);
+//lettersGuessed = [];
+//$("#lettersGuessed").html("Letters Guessed: " + "<br>" + lettersGuessed.join(" "));
+//$("#gameOutcome").empty();
+
+//playGame();
+
+
+//}
